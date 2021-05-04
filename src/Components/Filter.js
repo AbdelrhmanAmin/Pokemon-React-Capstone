2ea42dc8e27f2c8d093ac84f0617f1aed0c80721
AbdelrhmanAmin <maxhd213@gmail.com>
2021-05-04 17:53:19 +0200

    testing

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -1,15 +1,15 @@
 import { useDispatch } from 'react-redux';
 import { pokeFilter } from '../Actions/action';
-import '../btn.scss';
+
 const Filter = () => {
   const dispatch = useDispatch();
   return (
     <form>
-      <div class="form__group field">
-        <input type="input" class="form__field" placeholder="Name" name="name" id='name' required onChange={(e) => {
+      <div className="form__group field">
+        <input type="input" className="form__field" placeholder="Name" name="name" id='name' required onChange={(e) => {
           dispatch(pokeFilter(e.target.value))
         }} />
-        <label for="name" class="form__label">Pika! Pika!</label>
+        <label for="name" className="form__label">Pika! Pika!</label>
       </div>
     </form>
   )
---
+++ src/__tests__/Components/Main.test.js
@@ -0,0 +1,5 @@
+import { render, screen, cleanup } from '@testing-library/react';
+
+test("match", () => {
+  expect(true).toBe(true);
+})
